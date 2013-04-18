a7d9f7a1bd456f597cc390493eda82112a3ac011
ka <ka.kaosf@gmail.com>
2013-04-18 23:36:20 +0900

    finish lesson 10

--- js/app.js
+++ js/app.js
@@ -15,12 +15,6 @@ var task = new Task();
 
 var TaskView = Backbone.View.extend({
   tagName: 'li',
-  events: {
-    "click .command": "sayHello"
-  },
-  sayHello: function() {
-    alert('hello!');
-  },
   template: _.template($('#task-template').html()),
   render: function() {
     var template = this.template(this.model.toJSON());
@@ -28,8 +22,24 @@ var TaskView = Backbone.View.extend({
     return this;
   }
 });
-var taskView = new TaskView({model: task});
-console.log(taskView.render().el);
-$('body').append(taskView.render().el);
+
+// Collection
+
+var Tasks = Backbone.Collection.extend({
+  model: Task
+});
+var tasks = new Tasks([
+  {
+    title: 'task1',
+    completed: true
+  },
+  {
+    title: 'task2'
+  },
+  {
+    title: 'task3'
+  }
+]);
+console.log(tasks.toJSON());
 
 })();
