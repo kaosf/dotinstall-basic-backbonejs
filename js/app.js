94980ac89b5e129c165aa4af8e357e0ff7cc627d
ka <ka.kaosf@gmail.com>
2013-04-18 23:49:38 +0900

    finish lesson 13

--- js/app.js
+++ js/app.js
@@ -1,15 +1,28 @@
 (function () {
 
 var Task = Backbone.Model.extend({
+  defaults: {
+    title: 'do something',
+    completed: false
+  }
 });
 var Tasks = Backbone.Collection.extend({model: Task});
 
-var TaskView = Backbone.View.extend({});
+var TaskView = Backbone.View.extend({
+  tagName: 'li',
+  template: _.template($('#task-template').html());
+  render: function() {
+    var template = this.template(this.model.toJSON());
+    this.$el.html(template);
+    return this;
+  }
+});
+
 var TasksView = Backbone.View.extend({});
 
 var tasks = new Tasks([
   {
-    title: 'task1'
+    title: 'task1',
     completed: true
   },
   {
