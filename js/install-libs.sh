bceb990751253e9c357a2e008091e610c8db7ecc
ka <ka.kaosf@gmail.com>
2013-04-18 23:05:40 +0900

    finish lesson 02

---
+++ js/install-libs.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+wget https://raw.github.com/documentcloud/underscore/1.4.4/underscore.js
+wget http://code.jquery.com/jquery-1.9.1.js
+cp jquery-1.9.1.js jquery.js
+wget https://raw.github.com/documentcloud/backbone/1.0.0/backbone.js
