438c74deef02cabc09508a4b8b72153d9c827204
ka <ka.kaosf@gmail.com>
2013-04-19 01:01:19 +0900

    rewrite with CoffeeScript

---
+++ js/compile.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+coffee -c app.coffee
--- js/install-libs.sh
+++ js/install-libs.sh
@@ -4,3 +4,5 @@ wget https://raw.github.com/documentcloud/underscore/1.4.4/underscore.js
 wget http://code.jquery.com/jquery-1.9.1.js
 cp jquery-1.9.1.js jquery.js
 wget https://raw.github.com/documentcloud/backbone/1.0.0/backbone.js
+
+npm install -g coffee-script@1.6.2
