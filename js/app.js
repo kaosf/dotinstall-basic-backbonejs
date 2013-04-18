2019b959e3d2495f975c012cf538770646c2cd7b
ka <ka.kaosf@gmail.com>
2013-04-18 23:57:39 +0900

    finish lesson 15

--- js/app.js
+++ js/app.js
@@ -10,6 +10,20 @@ var Tasks = Backbone.Collection.extend({model: Task});
 
 var TaskView = Backbone.View.extend({
   tagName: 'li',
+  initialize: function() {
+    this.model.on('destroy', this.remove, this);
+  },
+  events: {
+    'click .delete': 'destroy'
+  },
+  destroy: function() {
+    if (confirm('are you sure?')) {
+      this.model.destroy();
+    }
+  },
+  remove: function() {
+    this.$el.remove();
+  },
   template: _.template($('#task-template').html()),
   render: function() {
     var template = this.template(this.model.toJSON());
